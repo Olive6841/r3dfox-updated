55c4534d0b24ce15f32c31d69b4cb3d8059d40e4
Bruno Freitas <48385307+angelbruni@users.noreply.github.com>
2024-03-18 17:20:32 +0000

    Improvements for today's showcase
    - Added code and style for profile pictures in titlebar, it has three modes "Geckium.profilepic.mode":
    	- "firefox": picks up your Mozilla account profile picture if you are logged in;
    	- "0" - "25": choose between the icons available from Chrome;
    	- "custom": allows for a custom image, simply paste the path to the image in the pref "Geckium.profilepic.path".
    - Removed border from menu buttons in custom theme for Chrome 11+;
    - Fixed new tab button background colour not showing when custom theme is on;
    - Improved Chrome 21 tabs accuracy but still needs more work;
    - Fixed tab hover and glare effect;
    - Fixed custom tabs text colour not being picked up in custom themes;
    - Fixed private browsing titlebar gradient for classic theme not working in Chrome 6+;
    - Switched the close button image to the one from Chrome 5, still needs to be changed for the other eras;
    - Added Home button icon.

---
+++ Profile Folder/chrome/JS/Geckium_people.uc.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Geckium - People
+// @author		AngelBruni
+// @loadorder   3
+// ==/UserScript==
+
+function setProfilePic() {
+	const attr = "profilepic";
+	const prefSetting = pref("Geckium.profilepic.mode").tryGet.string();
+
+	if (prefSetting == "" || pref == "firefox") {
+		docElm.setAttribute(attr, "firefox");
+	} else if (prefSetting == "custom") {
+		docElm.setAttribute(attr, "custom");
+		docElm.style.setProperty("--custom-profile-picture", "url(file:///" + pref('Geckium.profilepic.path').tryGet.string().replace(/\\/g, "/").replace(" ", "%20") + ")");
+	} else {
+		docElm.setAttribute(attr, prefSetting);
+	}
+}
+
+/* bruni: Automatically apply a profile picture 
+		  when it detecs changes in the pref. */
+const profilePictureObserver = {
+	observe: function (subject, topic, data) {
+		if (topic == "nsPref:changed") {
+			setProfilePic()
+		}
+	}
+};
+Services.prefs.addObserver("Geckium.profilepic.mode", profilePictureObserver, false)
+Services.prefs.addObserver("Geckium.profilepic.path", profilePictureObserver, false)
+window.addEventListener("load", setProfilePic);
