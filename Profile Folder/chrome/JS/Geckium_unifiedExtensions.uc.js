458ae57821178e3605722b6fe370c42018516544
Bruno Freitas <48385307+angelbruni@users.noreply.github.com>
2024-03-23 13:53:03 +0000

    Hide unified extensions button option

---
+++ Profile Folder/chrome/JS/Geckium_unifiedExtensions.uc.js
@@ -0,0 +1,33 @@
+function hideUnifiedExtensionsButton() {
+	const navBar = document.getElementById("nav-bar");
+	const unifiedExtensionsButton = document.getElementById("unified-extensions-button");
+	const navBarCustomizationTarget = document.getElementById("nav-bar-customization-target");
+	const urlbarBackground = document.getElementById("urlbar-background");
+
+	if (pref("Geckium.unifiedExtensions.hide").tryGet.bool()) {
+		navBar.setAttribute("unifiedextensions", "hide");
+		insertAfter(unifiedExtensionsButton, urlbarBackground);
+		
+	} else {
+		navBar.removeAttribute("unifiedextensions");
+		navBarCustomizationTarget.appendChild(unifiedExtensionsButton);
+	}
+}
+
+window.addEventListener("load", hideUnifiedExtensionsButton);
+
+// FIXME: Find the correct event instead of using a timeout initially.
+setTimeout(() => {
+	hideUnifiedExtensionsButton();
+}, 50);
+
+/* bruni: Automatically apply appearance and theme
+		  attributes when it detecs changes in the pref. */
+const unifiedExtensionsObserver = {
+	observe: function (subject, topic, data) {
+		if (topic == "nsPref:changed") {
+			hideUnifiedExtensionsButton();
+		}
+	},
+};
+Services.prefs.addObserver("Geckium.unifiedExtensions.hide", unifiedExtensionsObserver, false);
