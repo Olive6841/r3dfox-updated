9e3a2f3807598b0218a06c2d6d9279576da14e3f
nabrious0 <nabrious+1@icloud.com>
2024-03-23 13:53:08 +0100

    hopefully the last shit with tab size 2 issue

--- Profile Folder/chrome/JS/Geckium_appearance.uc.js
+++ Profile Folder/chrome/JS/Geckium_appearance.uc.js
@@ -6,128 +6,146 @@
 // ==/UserScript==
 
 const appearanceMap = {
-	0: "five",
-	1: "six",
-	2: "eleven",
-	3: "twentyone",
-	4: "fortyseven",
-	5: "fifty"
+    0: "five",
+    1: "six",
+    2: "eleven",
+    3: "twentyone",
+    4: "twentysix",
+    5: "fortyseven",
+    6: "fifty",
 };
 
 function applyApperance(choice) {
-	// bruni: We get the first and last available keys so
-	//		  we don't hardcode the values in the code.
-	const mapKeys = Object.keys(appearanceMap).map(Number);
-	const firstKey = Math.min(...mapKeys);
-	const lastKey = Math.max(...mapKeys);
+    // bruni: We get the first and last available keys so
+    //		  we don't hardcode the values in the code.
+    const mapKeys = Object.keys(appearanceMap).map(Number);
+    const firstKey = Math.min(...mapKeys);
+    const lastKey = Math.max(...mapKeys);
 
-	const prefChoice = pref(prefMap.appearance).tryGet.int();
+    const prefChoice = pref(prefMap.appearance).tryGet.int();
 
-	// bruni: Let's remove all appearance attributes first.
-	const pastAttrs = docElm.getAttributeNames();
-    pastAttrs.forEach(attr => {
-        if (attr.startsWith("geckium-"))
-            docElm.removeAttribute(attr);
+    // bruni: Let's remove all appearance attributes first.
+    const pastAttrs = docElm.getAttributeNames();
+    pastAttrs.forEach((attr) => {
+        if (attr.startsWith("geckium-")) docElm.removeAttribute(attr);
     });
 
-	// bruni: Let's apply the correct appearance attributes.
-	if (typeof choice == "undefined") {
-		if (prefChoice > lastKey) {
-			choice = lastKey;
-		} else if (prefChoice < firstKey || prefChoice == null) {
-			choice = firstKey;
-		} else {
-			choice = pref(prefMap.appearance).tryGet.int();
-		}
-	}
+    // bruni: Let's apply the correct appearance attributes.
+    if (typeof choice == "undefined") {
+        if (prefChoice > lastKey) {
+            choice = lastKey;
+        } else if (prefChoice < firstKey || prefChoice == null) {
+            choice = firstKey;
+        } else {
+            choice = pref(prefMap.appearance).tryGet.int();
+        }
+    }
 
-	for (let i = 0; i <= choice; i++) {
+    for (let i = 0; i <= choice; i++) {
         if (appearanceMap[i]) {
             const attr = "geckium-" + appearanceMap[i];
             docElm.setAttribute(attr, "");
         }
     }
 
-	// bruni: Let's also apply the attribute specific to the
-	//		  user choice so we can make unique styles for it.
-	docElm.setAttribute("geckium-choice", appearanceMap[choice]);
+    // bruni: Let's also apply the attribute specific to the
+    //		  user choice so we can make unique styles for it.
+    docElm.setAttribute("geckium-choice", appearanceMap[choice]);
 
-	dispatchEvent(appearanceChanged);
+    dispatchEvent(appearanceChanged);
 }
 window.addEventListener("load", applyApperance);
 
 // FIXME: Find the correct event instead of using a timeout initially.
 setTimeout(() => {
-	applyApperance();
+    applyApperance();
 }, 50);
 
 function setThemeAttr() {
-	docElm.setAttribute("lwtheme-id", pref("extensions.activeThemeID").tryGet.string())
+    docElm.setAttribute(
+        "lwtheme-id",
+        pref("extensions.activeThemeID").tryGet.string()
+    );
 
-	if (pref("extensions.activeThemeID").tryGet.string().includes("default-theme")) {
-		docElm.setAttribute("chromemargin", "0,3,3,3");
-	} else if (pref("extensions.activeThemeID").tryGet.string().includes("firefox-compact")) {
-		docElm.setAttribute("chromemargin", "0,3,3,3");
-	} else {
-		let customThemeMode;
+    if (
+        pref("extensions.activeThemeID")
+            .tryGet.string()
+            .includes("default-theme")
+    ) {
+        docElm.setAttribute("chromemargin", "0,3,3,3");
+    } else if (
+        pref("extensions.activeThemeID")
+            .tryGet.string()
+            .includes("firefox-compact")
+    ) {
+        docElm.setAttribute("chromemargin", "0,3,3,3");
+    } else {
+        let customThemeMode;
 
-		if (pref("Geckium.customtheme.mode").tryGet.int() <= 0) {
-			customThemeMode = 0;
-			docElm.setAttribute("chromemargin", "0,0,0,0")
-		} else if (pref("Geckium.customtheme.mode").tryGet.int() == 1) {
-			customThemeMode = 1;
-			docElm.setAttribute("chromemargin", "0,0,0,0")
-		} else if (pref("Geckium.customtheme.mode").tryGet.int() >= 2) {
-			customThemeMode = 2;
-			docElm.setAttribute("chromemargin", "0,3,3,3")
-		}
+        if (pref("Geckium.customtheme.mode").tryGet.int() <= 0) {
+            customThemeMode = 0;
+            docElm.setAttribute("chromemargin", "0,0,0,0");
+        } else if (pref("Geckium.customtheme.mode").tryGet.int() == 1) {
+            customThemeMode = 1;
+            docElm.setAttribute("chromemargin", "0,0,0,0");
+        } else if (pref("Geckium.customtheme.mode").tryGet.int() >= 2) {
+            customThemeMode = 2;
+            docElm.setAttribute("chromemargin", "0,3,3,3");
+        }
 
-		docElm.setAttribute("customthememode", customThemeMode);
-	}
+        docElm.setAttribute("customthememode", customThemeMode);
+    }
 
-	if (navigator.userAgent.includes("Windows NT 10.0") && !window.matchMedia('(-moz-ev-native-controls-patch)').matches) {
-		docElm.setAttribute("chromemargin", "0,0,0,0");
-	}
-	
-	if (!window.matchMedia('(-moz-windows-compositor: 1)').matches) {
-		docElm.setAttribute("chromemargin", "0,0,0,0");
-	}
+    if (
+        navigator.userAgent.includes("Windows NT 10.0") &&
+        !window.matchMedia("(-moz-ev-native-controls-patch)").matches
+    ) {
+        docElm.setAttribute("chromemargin", "0,0,0,0");
+    }
+
+    if (!window.matchMedia("(-moz-windows-compositor: 1)").matches) {
+        docElm.setAttribute("chromemargin", "0,0,0,0");
+    }
 }
 
 const themeObserver = {
-	observe: function (subject, topic, data) {
-		if (topic == "nsPref:changed")
-		setThemeAttr();
-	}
+    observe: function (subject, topic, data) {
+        if (topic == "nsPref:changed") setThemeAttr();
+    },
 };
 window.addEventListener("load", setThemeAttr);
-Services.prefs.addObserver("extensions.activeThemeID", themeObserver, false)
+Services.prefs.addObserver("extensions.activeThemeID", themeObserver, false);
 
 const customThemeModeObserver = {
-	observe: function (subject, topic, data) {
-		if (topic == "nsPref:changed")
-		setThemeAttr();
-	}
+    observe: function (subject, topic, data) {
+        if (topic == "nsPref:changed") setThemeAttr();
+    },
 };
 window.addEventListener("load", setThemeAttr);
-Services.prefs.addObserver("Geckium.customtheme.mode", customThemeModeObserver, false)
+Services.prefs.addObserver(
+    "Geckium.customtheme.mode",
+    customThemeModeObserver,
+    false
+);
 
 /* bruni: Automatically apply appearance and theme
 		  attributes when it detecs changes in the pref. */
 const appearanceObserver = {
-	observe: function (subject, topic, data) {
-		if (topic == "nsPref:changed") {
-			applyApperance();
-			setThemeAttr();
-		}
-	}
+    observe: function (subject, topic, data) {
+        if (topic == "nsPref:changed") {
+            applyApperance();
+            setThemeAttr();
+        }
+    },
 };
-Services.prefs.addObserver(appearanceMap.appearance, appearanceObserver, false)
+Services.prefs.addObserver(appearanceMap.appearance, appearanceObserver, false);
 
 function changePrivateBadgePos() {
-	const privateBrowsingIndicatorWithLabel = document.getElementById("private-browsing-indicator-with-label");
-	const titlebarSpacer = document.querySelector(".titlebar-spacer");
+    const privateBrowsingIndicatorWithLabel = document.getElementById(
+        "private-browsing-indicator-with-label"
+    );
+    const titlebarSpacer = document.querySelector(".titlebar-spacer");
 
-	insertBefore(privateBrowsingIndicatorWithLabel, titlebarSpacer);
+    insertBefore(privateBrowsingIndicatorWithLabel, titlebarSpacer);
 }
 window.addEventListener("load", changePrivateBadgePos);
