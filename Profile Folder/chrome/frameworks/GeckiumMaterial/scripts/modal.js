4b5557b5843ec6deeb7f34cea7c393adfc3483ac
Nabrious <nabrious+1@icloud.com>
2024-04-05 00:08:23 +0100

    Dialogs and icon changes

---
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/modal.js
@@ -0,0 +1,32 @@
+let modalToggles = document.querySelectorAll('[data-toggle-modal]');
+let modals = document.querySelectorAll('[data-modal]');
+let mainWindow = document.querySelector('#window');
+
+modalToggles.forEach(modalToggle => {
+  modalToggle.addEventListener('click', function() {
+    let modalId = modalToggle.dataset.toggleModal;
+    let modalTarget = document.querySelector(`[data-modal="${modalId}"]`);
+
+    if (modalTarget.classList.contains('active')) {
+      modalTarget.classList.remove('active');
+    } else {
+      modalTarget.classList.add('active');
+    }
+  });
+});
+
+modals.forEach(modal => {
+    modal.addEventListener('click', function(event) {
+        if (!event.target.closest('.card')) {
+            if (modal.classList.contains('active')) {
+              modal.classList.remove('active');
+            }
+        }
+    });
+});
+
+window.addEventListener('load', function() {
+  modals.forEach(modal => {
+    mainWindow.appendChild(modal);
+  });
+});
