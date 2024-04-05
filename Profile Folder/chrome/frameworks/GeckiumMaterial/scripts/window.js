4fca9cd99c80ede08c126273bedaccc2a536ae7d
Nabrious <nabrious+1@icloud.com>
2024-04-05 03:34:41 +0100

    caption buttons!

--- Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/window.js
+++ Profile Folder/chrome/frameworks/GeckiumMaterial/scripts/window.js
@@ -3,3 +3,29 @@ function windowTitle() {
 }
 
 document.addEventListener("DOMContentLoaded", windowTitle);
+
+let maximizeButtons = document.querySelectorAll(".maximize");
+let restoreButtons = document.querySelectorAll(".restore");
+
+window.addEventListener('load', function() {
+	restoreButtons.forEach(restoreButton => {
+		restoreButton.style.display = "none";
+	})
+});
+
+maximizeButtons.forEach(maximizeButton => {
+    maximizeButton.addEventListener("click", () => {
+        maximizeButton.style.display = "none";
+		restoreButtons.forEach(restoreButton => {
+			restoreButton.style.display = "flex";
+		})
+    });
+});
+restoreButtons.forEach(restoreButton => {
+    restoreButton.addEventListener("click", () => {
+        restoreButton.style.display = "none";
+		maximizeButtons.forEach(maximizeButton => {
+			maximizeButton.style.display = "flex";
+		})
+    });
+});
