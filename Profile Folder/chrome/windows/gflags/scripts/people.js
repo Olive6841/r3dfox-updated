26f99456c6f9294a9f3726f63bf330c09fc4348c
Bruno Freitas <48385307+angelbruni@users.noreply.github.com>
2024-04-19 20:59:38 +0100

    Geckium Flags - People settings now work

--- Profile Folder/chrome/windows/gflags/scripts/people.js
+++ Profile Folder/chrome/windows/gflags/scripts/people.js
@@ -1,3 +1,18 @@
+function updateProfilePictureSettingsState() {
+	const container = document.getElementById("profile-pic-content");
+
+	container.setAttribute("profile-pic-button", pref("Geckium.profilepic.button").tryGet.bool())
+}
+const profilePictureStateObserver = {
+	observe: function (subject, topic, data) {
+		if (topic == "nsPref:changed") {
+			updateProfilePictureSettingsState()
+		}
+	}
+};
+Services.prefs.addObserver("Geckium.profilepic.button", profilePictureStateObserver, false)
+document.addEventListener("DOMContentLoaded", updateProfilePictureSettingsState);
+
 function updateProfilePictures() {
 	const menu = document.getElementById("pfp-mode-select");
 
@@ -7,16 +22,37 @@ function updateProfilePictures() {
 	const chromePfPs = 25;
 
 	for (let i = 0; i <= chromePfPs; i++) {
-		const PfPBtn = document.createElement("button");
-		PfPBtn.classList.add("people-picture", "ripple-enabled");
+		const pfPRadio = document.createElement("input");
+		pfPRadio.type = "radio";
+		pfPRadio.name = "pfp-chromium"
+		pfPRadio.classList.add("people-picture", "ripple-enabled");
 
-		const PfPImage = document.createXULElement("image");
-		PfPImage.setAttribute("src", "chrome://userchrome/content/assets/img/profile/" + i + ".png")
-		PfPBtn.appendChild(PfPImage);
+		pfPRadio.addEventListener("click", () => {
+			pref("Geckium.profilepic.chromiumIndex").set.int(i);
+		})
 
-		gridPfPs.appendChild(PfPBtn);
+		const pfPImage = document.createXULElement("image");
+		pfPImage.setAttribute("src", "chrome://userchrome/content/assets/img/profile/" + i + ".png")
+		pfPRadio.appendChild(pfPImage);
+
+		gridPfPs.appendChild(pfPRadio);
 	}
 
 	menu.addEventListener("change", updateProfilePictures);
 }
 document.addEventListener("DOMContentLoaded", updateProfilePictures);
+
+function updateProfilePictureChoiceOption() {
+	const container = document.getElementById("card-avatar");
+
+	container.setAttribute("profile-pic-mode", pref("Geckium.profilepic.mode").tryGet.int());
+}
+const profilePictureObserver = {
+	observe: function (subject, topic, data) {
+		if (topic == "nsPref:changed") {
+			updateProfilePictureChoiceOption()
+		}
+	}
+};
+Services.prefs.addObserver("Geckium.profilepic.mode", profilePictureObserver, false)
+document.addEventListener("DOMContentLoaded", updateProfilePictureChoiceOption);
---
+++ Profile Folder/chrome/windows/gflags/scripts/settingsLoader.js
@@ -0,0 +1,45 @@
+function loadSelectorSetting() {
+	document.querySelectorAll("button.menu[data-pref]").forEach(selector => {
+		let current;
+
+		const checkInt = selector.querySelector(".list .item");
+		if (Number.isInteger(parseInt(checkInt.getAttribute("value"))))
+			current = pref(`Geckium.${selector.dataset.pref}`).tryGet.int();
+		else
+			current = pref(`Geckium.${selector.dataset.pref}`).tryGet.string();
+
+		selector.setValue(current);
+
+		selector.querySelectorAll(".list .item").forEach(item => {
+			item.addEventListener("click", () => {
+				if (Number.isInteger(parseInt(item.getAttribute("value"))))
+					pref(`Geckium.${selector.dataset.pref}`).set.int(parseInt(item.getAttribute("value")));
+				else
+					pref(`Geckium.${selector.dataset.pref}`).set.string(`${item.getAttribute("value")}`);
+			})
+		})
+	})
+}
+document.addEventListener("DOMContentLoaded", loadSelectorSetting);
+
+function loadTextFieldSetting() {
+	document.querySelectorAll('input[type="text"][data-pref]').forEach(input => {
+		input.value = pref(`Geckium.${input.dataset.pref}`).tryGet.string()
+
+		input.addEventListener("input", () => {
+			pref(`Geckium.${input.dataset.pref}`).set.string(input.value);
+		})
+	})
+}
+document.addEventListener("DOMContentLoaded", loadTextFieldSetting);
+
+function loadSwitchSetting() {
+	document.querySelectorAll('input.switch[data-pref]').forEach(input => {
+		input.checked = pref(`Geckium.${input.dataset.pref}`).tryGet.bool();
+
+		input.addEventListener("input", () => {
+			pref(`Geckium.${input.dataset.pref}`).set.bool(input.checked);
+		})
+	})
+}
+document.addEventListener("DOMContentLoaded", loadSwitchSetting);
